25cabda000afb81c8f26ea9c9398ca1d4f93311d
Caio Querino <caioquerino04@gmail.com>
2025-08-21 11:17:15 -0300

    atualiza imagem de perfil para modo escuro e ajusta função de alternância de temas

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,12 @@
-
 function toggleMode () {
   const html = document.documentElement
+  const profileImage = document.querySelector('#profile img')
+
   html.classList.toggle('light')
+
+  if (profileImage) {
+    profileImage.setAttribute('src', html.classList.contains('light') ? './src/assets/profile-light.png' : './src/assets/profile-dark.jpeg')
+  } else {
+    console.error("Profile image not found. Check the selector and HTML structure.");
+  }
 }
